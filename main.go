8f7182c08181461b8422259ac106e3cd386df495
Jae Kwon <jkwon.work@gmail.com>
2013-08-06 21:39:36 -0700

    .

--- main.go
+++ main.go
@@ -1,42 +1,66 @@
 package main
 
 import "fmt"
-//import . "github.com/jaekwon/go-prelude"
+//import "reflect"
+import . "github.com/jaekwon/go-prelude"
 
 type StorageUnit uint
-type Error uint
+type Status uint
+type Data []byte
 
-type Data interface {
+/*
+ * The Storage interface is an Gourami flavored interface to the OS filesystem.
+ * We use the native file system handles defragmentation issues.
+ * Since filesystems typically support a maximum number of files per directory,
+ * we handle indexing these files via directory structures.
+ *
+ * NOTE: Consider checking dir_index to see when to split directories.
+ * Seems that we should do it around 20K files with dir_index, 5K without.
+ * Maybe better to measure performance and do what is optimal.
+ */
+type Storage interface {
+    Owner() *Entity
+    Size() (used StorageUnit, total StorageUnit)
+    Store(*Serializable, chan Status)
 }
+
+type Serializable interface {
+    Serialize() Data
+}
+
+/**
+ * Entity
+ */
 
 type Entity struct {
     name string
 }
 
-type StorageA struct {
+func (this *Entity) Serialize() Data {
+    return Data(fmt.Sprintf("<Entity %s>", this.name))
 }
 
-func (*StorageA) Owner () *Entity {
-    return nil
-}
 
-func (*StorageA) Store (data *Data, trig chan bool) *Error {
-    return nil
-}
+/**
+ * Mail struct represents Data for some entity
+ */
 
-type Storage interface {
-    Owner() *Entity
-    Store(*Data, chan bool) *Error
-}
-
-// The Mail struct represents a piece of Data for somebody
 type Mail struct {
     from Entity
     to Entity
-    // headers Dict
-    data Data
+    headers Dict
+    body Data
+}
+
+func (*Mail) Serialize() Data {
+    // serialize everything, including internal body data
+    return Data("<Mail>")
 }
 
 func main () {
     fmt.Println("Gourami \033[0;34m<°\\\\<\033[0m (version 0.0)")
+
+    user := Entity{name:"Jae"}
+    fmt.Println(user)
+    fmt.Println(string(user.Serialize()))
 }
